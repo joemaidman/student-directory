8db5eafcf254e8b287c02055b0d85a5100baeac4
Joe Maidman <joemaidman@gmail.com>
2017-02-01 12:58:45 +0000

    Refactored directory with array and iteration

--- directory.rb
+++ directory.rb
@@ -1,17 +1,22 @@
-student_count = 11
-# Print a list of students to the screen
+# put students into an array
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+# print the array
 puts "The students of Villians Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-# Print does not add a new line at the end. Puts does that.
-puts "Overall, we have #{student_count} great students"
+students.each do |student|
+  puts student
+end
+# Print the total
+puts "Overall, we have #{students.count} great students"
