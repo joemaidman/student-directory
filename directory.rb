58f995fb27d9dea6297acb79d21f866520675f4f
Joe Maidman <joemaidman@gmail.com>
2017-02-01 12:47:57 +0000

    Added count of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# Print a list of students to the screen
 puts "The students of Villians Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# Print does not add a new line at the end. Puts does that.
+print "Overall, we have "
+print 11
+puts " great students"
