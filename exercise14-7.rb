779da93c36103d0618d48db3fa8259af47ad279f
Joe Maidman <joemaidman@gmail.com>
2017-02-06 11:23:13 +0000

    Exercise 14-7

---
+++ exercise14-7.rb
@@ -0,0 +1,239 @@
+# Refactoring to use Ruby's CSV library instead of File
+require 'csv'
+# Setup global variables
+@students = []
+@defaultList = false
+@defaultFileName = "students.csv"
+@months = [:January, :February, :March, :April, :May, :June, :July, :August, :September, :October, :November, :December]
+def default_students
+# A hash of default students which is an option for the user to load
+@students = [
+  {name: "Dr. Hannibal Lecter",cohort: :november,  hobby: :sport, weight: 87, age: 64},
+  {name: "Darth Vader", cohort: :november,  hobby: :reading, weight: 87, age: 53},
+  {name: "Nurse Ratched", cohort: :november,  hobby: :music, weight: 100, age: 42},
+  {name: "Michael Corleone", cohort: :november,  hobby: :coding, weight: 68, age: 24},
+  {name: "Alex DeLarge", cohort: :november, hobby: :coding, weight: 130, age: 28},
+  {name: "The Wicked Witch of the West", cohort: :november ,hobby: :gaming, weight: 120, age: 43},
+  {name: "Terminator", cohort: :november, hobby: :music, weight: 72, age: 34},
+  {name: "Freddy Krueger", cohort: :november, hobby: :reading, weight: 55, age: 18},
+  {name: "The Joker", cohort: :november, hobby: :sport, weight: 30, age: 45},
+  {name: "Joffrey Baratheon", cohort: :november, hobby: :music, weight: 70, age: 30},
+  {name: "Norman Bates", cohort: :november, hobby: :music, weight: 80, age: 25}
+]
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+      puts "Finished inputting students."
+    when "2"
+      show_students
+      puts "Finished showing students."
+    when "3"
+      save_Students
+      puts "Finished saving students."
+    when "4"
+      load_students
+      puts "Finished loading students."
+    when "5"
+      load_default_students
+      puts "Finished loading default students."
+    when "9"
+      exit
+    else
+      "I didn't understand that command. Please try again."
+  end
+end
+
+def load_default_students
+  puts "WARNING - This will overwrite all students currently loaded."
+  puts "Continue? y/n"
+  continue = STDIN.gets.chomp.downcase
+  default_students unless continue == "n"
+end
+
+def print_menu
+  puts "What do you want to do?"
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list"
+  puts "4. Load a list"
+  puts "5. Load a default list of students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print
+  print_footer
+end
+
+def print_input_instrucs
+  puts("Please enter the names of the students.")
+  puts("To finish, just hit return twice.")
+  puts("If no names are entered, I will use the default list.")
+end
+
+def input_students
+  print_input_instrucs
+  # Get the first name
+  # Use gsub to substitute a string instead of chomp
+  name = STDIN.gets.gsub("\n","")
+  # While the name is not empty, repeat this code
+  while !name.empty? do
+    puts("Please enter #{name}'s cohort.'")
+    # Get the cohort from the user.
+    cohort = STDIN.gets.gsub("\n","")
+    check_valid_month(cohort)
+    # Add the student to the array
+    add_student(name, cohort)
+    puts("Now we have #{@students.count} #{plural_studs}.")
+    # Get another name from the user
+    puts("Please enter the next name.")
+    name = STDIN.gets.gsub("\n","")
+  end
+  set_default_students
+end
+
+def plural_studs
+  return "students" unless @students.count < 2
+end
+
+def set_default_students
+  if @students.count == 0 && @defaultList == true
+    @students = default_students
+  end
+end
+
+def print_header
+  print_break
+  puts("The students of Villians Academy")
+  print_break
+end
+
+def print_break
+  puts("-------------")
+end
+
+def check_valid_month(cohort)
+  # Look-up the user input for cohort in the list of allowed symbols (months of the year).
+  # Unti it is valid, ask the user to re-enter
+  while !@months.include?(cohort.to_sym)
+    puts("Couldn't find the \'#{cohort}\' cohort, please check your spelling and case!")
+    cohort = STDIN.gets.gsub("\n","")
+  end
+end
+
+def print
+    count = 0
+    while count < @students.count
+      puts("#{count + 1}. #{@students[count][:name]} (#{@students[count][:cohort]} cohort)")
+      count += 1
+    end
+end
+
+def print_by_first_letter(letter)
+  @students.each_with_index() do |student, index|
+    puts("#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)") if student[:name][0] == letter
+  end
+end
+
+def print_by_character_count(count)
+  @students.each_with_index() do |student, index|
+    puts("#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)") if student[:name].length < count
+  end
+end
+
+def print_footer
+  print_break
+  if @students.count == 0
+    puts("There are no students in the database :(")
+  else
+    puts("Overall, we have #{@students.count} great #{plural_studs}.")
+  end
+  print_break
+end
+
+def print_to_center(message)
+  puts message.center(50)
+end
+
+def print_by_cohort
+  @months.each do |cohort|
+    students_this_month = Array.new
+    @students.each do |student|
+      students_this_month.push(student) if student[:cohort] == cohort
+    end
+    print_break
+    puts("#{cohort.to_s.capitalize} (#{students_this_month.size} students)")
+    print_break
+    students_this_month.count > 0 ? print(students_this_month) : puts("There are no students in the #{cohort.to_s.capitalize} cohort")
+  end
+end
+
+def check_file_from_user
+  puts "Please enter a filename (without the extension)."
+  checkName = STDIN.gets.chomp
+  while checkName.to_s.empty?
+    puts "Sorry that is not a valid filename. Please try again"
+    checkName = STDIN.gets.chomp
+  end
+  checkName  << ".csv"
+end
+
+def save_Students()
+  saveName = check_file_from_user
+  CSV.open(saveName, "w") do |csv|
+    # Iterate over the array of students
+    @students.each do |student|
+      csv << [student[:name], student[:cohort]]
+    end
+  end
+  puts "Student directory saved to #{saveName}."
+end
+
+def load_students(loadName = nil)
+  loadName = check_file_from_user if loadName.to_s.empty?
+  while
+    !File.exists?(loadName)
+    puts "File does not exist."
+    loadName = check_file_from_user
+  end
+  CSV.foreach(loadName) do |row|
+    name = row[0]
+    cohort = row[1]
+    add_student(name, cohort)
+  end
+end
+
+def add_student(name, cohort)
+ @students << {name: name, cohort: cohort.to_sym}
+end
+
+def try_load_students
+  filename = ARGV.first
+  if filename.nil?
+    puts "No filename provided as start-up argument. Loading default students file from #{@defaultFileName}"
+    load_students(@defaultFileName)
+  else
+    if File.exists?(filename)
+      load_students(filename)
+      puts "Loaded #{@students.count} from #{filename}"
+    else
+      puts "Sorry, #{filename} dosen't exist."
+      exit
+    end
+  end
+end
+
+#nothing happens until we call the methods
+try_load_students
+interactive_menu
